45da603c0f9a9393d989430ed2bc79815dbb0da7
Rakesh Tembhurne <rakesh.tembhurne@pinnacle.in>
2024-10-09 20:42:38 +0530

    feat: Created a build

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "redirector",
+      script: "build/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
